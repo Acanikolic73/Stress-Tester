a9543e71040f750196d3bfe87eea05e4f23bfde4
Aleksandar Nikolić <acanikolic736@gmail.com>
2025-09-11 00:01:37 +0200

    Initial version of Stress Tester

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace StressTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboLanguage = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbBrute = new System.Windows.Forms.RichTextBox();
+            this.rtbOptimized = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stress Tester";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 6);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboLanguage
+            // 
+            this.comboLanguage.FormattingEnabled = true;
+            this.comboLanguage.Items.AddRange(new object[] {
+            "C++"});
+            this.comboLanguage.Location = new System.Drawing.Point(31, 98);
+            this.comboLanguage.Name = "comboLanguage";
+            this.comboLanguage.Size = new System.Drawing.Size(216, 24);
+            this.comboLanguage.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(279, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "▶ Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Brute Force Code";
+            // 
+            // rtbBrute
+            // 
+            this.rtbBrute.Location = new System.Drawing.Point(32, 200);
+            this.rtbBrute.Name = "rtbBrute";
+            this.rtbBrute.Size = new System.Drawing.Size(391, 185);
+            this.rtbBrute.TabIndex = 5;
+            this.rtbBrute.Text = "";
+            // 
+            // rtbOptimized
+            // 
+            this.rtbOptimized.Location = new System.Drawing.Point(33, 448);
+            this.rtbOptimized.Name = "rtbOptimized";
+            this.rtbOptimized.Size = new System.Drawing.Size(391, 193);
+            this.rtbOptimized.TabIndex = 7;
+            this.rtbOptimized.Text = "";
+            this.rtbOptimized.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 398);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Optimized Code";
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(463, 200);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(294, 441);
+            this.rtbOutput.TabIndex = 9;
+            this.rtbOutput.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(457, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 667);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtbOptimized);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbBrute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboLanguage);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboLanguage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbBrute;
+        private System.Windows.Forms.RichTextBox rtbOptimized;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StressTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AppendOutput(string output)
+        {
+            rtbOutput.AppendText(output + Environment.NewLine);
+            rtbOutput.SelectionStart = rtbOutput.Text.Length;
+            rtbOutput.ScrollToCaret();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.ForeColor = Color.FromArgb(51, 51, 51);
+            label2.ForeColor = Color.FromArgb(51, 51, 51);
+            label3.ForeColor = Color.FromArgb(51, 51, 51);
+            label4.ForeColor = Color.FromArgb(51, 51, 51);
+            panel1.BackColor = Color.FromArgb(74, 144, 226);
+            button1.BackColor = Color.FromArgb(74, 144, 226);
+            comboLanguage.SelectedIndex = 0;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string CompileCpp(string cppFile)
+        {
+            string exeFile = Path.ChangeExtension(cppFile, ".exe");
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("g++", cppFile + " -o " + exeFile)
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            using (Process process = Process.Start(processStartInfo))
+            {
+                string errors = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+                if(!string.IsNullOrEmpty(errors))
+                {
+                    throw new Exception("Compalation errors:\n" + errors);
+                }
+            }
+            return exeFile;
+        }
+
+        private string RunProgram(string exePath, string input)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(exePath)
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            using (Process process = Process.Start(processStartInfo))
+            {
+                process.StandardInput.WriteLine(input);
+                process.StandardInput.Close();
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                return output.Trim();
+            }
+        }
+
+        private string GenerateTestCase()
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(1, 10);
+            string testCase = n.ToString() + "\n";
+            for (int i = 1; i <= n; i++)
+            {
+                testCase += rnd.Next(1, 100) + " ";
+            }
+            return testCase.Trim();
+        }
+
+        private async Task StressTest(string bruteFile, string optimizedFile)
+        {
+            rtbOutput.ForeColor = Color.Gray;
+            for(int i = 1; i <= 100; i++)
+            {
+                //testing 100 inputs
+                AppendOutput("Running on test " + i + " ...\n");
+                string input = GenerateTestCase();
+                string outputBrute = await Task.Run(() => RunProgram(bruteFile, input));
+                string outputOpt = await Task.Run(() => RunProgram(optimizedFile, input));
+                if(outputBrute != outputOpt)
+                {
+                    rtbOutput.Clear();
+                    rtbOutput.ForeColor = Color.Red;
+                    AppendOutput("Mismatch found!\n");
+                    AppendOutput("Input:\n" + input + "\n");
+                    AppendOutput("Brute Output: " + outputBrute + "\n");
+                    AppendOutput("Optimized Output: " + outputOpt + "\n");
+                    return;
+                }
+                
+            }
+            rtbOutput.Clear();
+            rtbOutput.ForeColor = Color.Green;
+            AppendOutput("All tests passed!");
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Clear();
+            string bruteCode = rtbBrute.Text;     // Brute force code textbox
+            string optCode = rtbOptimized.Text;   // Optimized code textbox
+            string language = comboLanguage.SelectedItem.ToString();
+
+            if (string.IsNullOrWhiteSpace(bruteCode) || string.IsNullOrWhiteSpace(optCode) || language == null)
+            {
+                MessageBox.Show("Please paste both codes and select a language!");
+                return;
+            }
+
+            try
+            {
+                //make a temporary files brute and optimized
+                string bruteFile = Path.Combine(Path.GetTempPath(), "brute.cpp");
+                string optimizedFile = Path.Combine(Path.GetTempPath(), "opt.cpp");
+
+                // write text in file
+                File.WriteAllText(bruteFile, bruteCode);
+                File.WriteAllText(optimizedFile, optCode);
+
+                //compile and convert to exe file 
+                string bruteExe = CompileCpp(bruteFile);
+                string optExe = CompileCpp(optimizedFile);
+
+                await StressTest(bruteExe, optExe);
+
+                // delete temporary files
+                File.Delete(bruteExe);
+                File.Delete(optExe);
+            }
+            catch (Exception ex)
+            {
+                rtbOutput.AppendText("Error: " + ex.Message + "\n");
+            }
+        }
+    }
+}
